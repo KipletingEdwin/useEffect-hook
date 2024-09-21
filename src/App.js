be1924ecf86c3ac63434ede06baf4bc2e4f9e93e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-21 08:18:55 +0300

    onMount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [resourceType, setResourceType] = useState('posts')
+
+  console.log('render')
+
+  useEffect(()=>{
+    console.log('onMount')
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+
+    <button onClick={()=> setResourceType('posts')}>Posts</button>
+    <button onClick={()=> setResourceType('users')}>Users</button>
+    <button onClick={()=> setResourceType('comments')}>Comments</button>
     </div>
+
+    <h1>{resourceType}</h1>
+    </>
   );
 }
 
